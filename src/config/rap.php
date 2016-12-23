f01fee1684df553f14d6c9e36aca23e256d4e3a9
Sergejs Zablockis <zablose@gmail.com>
2016-12-23 22:45:38 +0000

    Useless comment deleted.

--- src/config/rap.php
+++ src/config/rap.php
@@ -2,15 +2,6 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Models
-    |--------------------------------------------------------------------------
-    |
-    | Models to be used.
-    |
-    */
-
     'models' => [
         'role'       => Zablose\Rap\Models\Role::class,
         'permission' => Zablose\Rap\Models\Permission::class,
