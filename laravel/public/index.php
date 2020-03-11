62eddeb13688c4b8c50e0740974abf75ac595df2
Sergejs Zablockis <zablose@gmail.com>
2020-03-11 15:50:44 +0000

    Merge vendor folders to run tests from package root and to get rid of IDE messages about multiple class definitions.

--- laravel/public/index.php
+++ laravel/public/index.php
@@ -7,6 +7,8 @@
  * @author   Taylor Otwell <taylor@laravel.com>
  */
 
+use Illuminate\Contracts\Http\Kernel;
+
 define('LARAVEL_START', microtime(true));
 
 /*
@@ -21,7 +23,7 @@ define('LARAVEL_START', microtime(true));
 |
 */
 
-require __DIR__.'/../vendor/autoload.php';
+require __DIR__.'/../../vendor/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -49,7 +51,8 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 |
 */
 
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+/** @var Kernel $kernel */
+$kernel = $app->make(Kernel::class);
 
 $response = $kernel->handle(
     $request = Illuminate\Http\Request::capture()
