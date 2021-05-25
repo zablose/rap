d72e09f6e9e5dc1a82e610f425103a724d15fb44
Sergejs Zablockis <zablose@gmail.com>
2021-05-25 12:50:31 +0100

    Code refactor to comply with PSR-12.

--- migrations/2016_12_04_114402_create_permissions_table.php
+++ migrations/2016_12_04_114402_create_permissions_table.php
@@ -8,16 +8,19 @@ class CreatePermissionsTable extends Migration
 {
     public function up(): void
     {
-        Schema::create('rap_permissions', function (Blueprint $table)
-        {
-            $table->charset   = 'utf8';
-            $table->collation = 'utf8_unicode_ci';
+        Schema::create(
+            'rap_permissions',
+            function (Blueprint $table)
+            {
+                $table->charset = 'utf8';
+                $table->collation = 'utf8_unicode_ci';
 
-            $table->increments('id');
-            $table->string('name', 80)->unique();
-            $table->mediumText('description')->nullable();
-            $table->timestamps();
-        });
+                $table->increments('id');
+                $table->string('name', 80)->unique();
+                $table->mediumText('description')->nullable();
+                $table->timestamps();
+            }
+        );
     }
 
     public function down(): void
