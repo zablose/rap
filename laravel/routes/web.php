9226ee4d07ce502616663412b90242eb6532578b
Sergejs Zablockis <zablose@gmail.com>
2025-06-26 12:39:26 +0100

    Rename welcome view to home, to avoid matching with the default view from Laravel skeleton - makes comparison easier.

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', fn() => view('welcome'));
+Route::get('/', fn() => view('home'));
 Route::get('/user', fn() => view('user'))->middleware('role:user');
 Route::post('/password/update', fn() => view('password-update'))->middleware('permission:password.update');
